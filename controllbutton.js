2e540ca9a11ec76df7fa009b763112faf6f1990b
Mahhungk21 <hung.phammanhk21@hcmut.edu.vn>
2024-05-04 01:08:49 +0700

    add login-homepage

--- controllbutton.js
+++ controllbutton.js
@@ -6,9 +6,20 @@ const listControl = document.querySelector(".home .list-control");
 autoButton.addEventListener("click", function () {
     // Hiển thị phần tử div
     listControl.style.display = "block"; // Hiển thị phần tử
+    autoButton.classList.remove("btn-danger");
+    autoButton.classList.add("btn-primary");
+    //
+    manualButton.classList.remove("btn-primary");
+    manualButton.classList.add("btn-danger");
 });
 // click vào nút "Thủ công"
 manualButton.addEventListener("click", function () {
     // Ẩn đi phần tử div
     listControl.style.display = "none"; // Ẩn đi phần tử
+
+    manualButton.classList.remove("btn-danger");
+    manualButton.classList.add("btn-primary");
+    //
+    autoButton.classList.remove("btn-primary");
+    autoButton.classList.add("btn-danger");
 });
